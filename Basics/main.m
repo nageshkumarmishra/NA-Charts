5bb234e33878fa6cf3ef90bc87e444d0fd8316fb
iSparrow Labs <iSparrowLabs@iSparrows-MacBook-Pro.local>
2014-01-02 11:18:49 +0530

    Initial Commit

---
+++ Basics/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Basics
+//
+//  Created by iSparrow Labs on 02/01/14.
+//  Copyright (c) 2014 iSparrow Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
